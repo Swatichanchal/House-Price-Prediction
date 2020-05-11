8200c897f3a3d60a79d97583830ee0c22cb7f64f
Swati Chanchal <34857622+Swatichanchal@users.noreply.github.com>
2020-05-11 22:17:09 +0530

    Add files via upload

---
+++ House_pricing/price_DT.py
@@ -0,0 +1,251 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Importing the dataset
+data = pd.read_csv(r'C:\Users\Swati\Downloads\data.csv')
+'''
+X = data.iloc[:, 1:-1].values
+y = data.iloc[:, -1].values
+'''
+X = data.iloc[:, 1:-1]
+y = data.iloc[:, -1]
+print(X.shape)
+
+# REMOVING A ROW FROM TRAINING SET 
+dataset.columns.get_loc('Condition1')
+dataset.columns.get_loc('GarageQual')
+dataset.columns.get_loc('Street')
+dataset.columns.get_loc('YearBuilt')
+'''
+X = np.delete(X,13 , 1 )
+X = np.delete(X,63 , 1 )
+X = np.delete(X,5 , 1 )
+X = np.delete(X,19 , 1 )
+# DELETE IS NUMPY FUNCTION 
+'''
+X = X.drop(['Condition1','Street','YearBuilt'] , axis=1)
+print(X.shape)
+print('The X set shape is : %s' %str(X.shape))
+print('The y set shape is : %s' %str(y.shape))
+print('The data set shape is : %s' %str(data.shape))
+
+'''
+# Determines all the unique element in the array
+np.unique(X[: , 2])
+
+# Determine the LENGTH of the unique element present in the array 
+len(np.unique(X[: , 17]))     
+X = np.delete(X, 17 , 1 )
+'''
+'''
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+
+X[:, 2] = labelencoder.fit_transform(X[:, 2])
+X[:, 2].reshape(-1, 1)
+onehotencoder = OneHotEncoder(categorical_features = [147])
+X = onehotencoder.fit_transform(X[:, 2]).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+'''
+
+'''
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+'''
+
+# Imputing the missing values
+missing =X.isna().sum(axis=0).sort_values(ascending=False)
+missing_value_columns = missing[missing>0]
+print('They are %s columns with missing values : \n%s ' %(missing_value_columns.count() , [(index , value) for (index , value) in missing_value_columns.iteritems()]))
+
+def impute_value(X):
+    dataset =X
+    dataset['PoolQC'].fillna('NA' , inplace = True)
+    dataset['MiscFeature'].fillna('NA' , inplace = True)
+    dataset['Alley'].fillna('NA' , inplace = True)
+    dataset['Fence'].fillna('NA' , inplace = True)
+    dataset['FireplaceQu'].fillna('NA' , inplace = True)
+    dataset['LotFrontage'].fillna(dataset['LotFrontage'].median() , inplace = True)
+    dataset['GarageCond'].fillna('NA' , inplace = True)
+    dataset['GarageType'].fillna('NA' , inplace = True)
+    dataset['GarageYrBlt'].fillna(dataset['GarageYrBlt'].median() , inplace = True)
+    dataset['GarageFinish'].fillna('NA' , inplace = True)
+    dataset['GarageQual'].fillna('NA' , inplace = True)
+    dataset['BsmtExposure'].fillna('NA' , inplace = True)
+    dataset['BsmtFinType2'].fillna('NA' , inplace = True)
+    dataset['BsmtFinType1'].fillna('NA' , inplace = True)
+    dataset['BsmtCond'].fillna('NA' , inplace = True)
+    dataset['BsmtQual'].fillna('NA' , inplace = True)
+    dataset['MasVnrArea'].fillna(dataset['MasVnrArea'].median() , inplace = True)
+    dataset['MasVnrType'].fillna('None' , inplace = True)
+    dataset['Electrical'].fillna('SBrkr' , inplace = True)
+    return dataset
+
+X = impute_value(X)
+
+# Cross checking the missing value
+missing =X.isna().sum(axis=0).sort_values(ascending=False)
+missing_value_columns = missing[missing>0]
+print('They are %s columns with missing values : \n%s ' %(missing_value_columns.count() , [(index , value) for (index , value) in missing_value_columns.iteritems()]))
+
+# to check the missing value is present or not
+print(X.isnull())
+print(X.isnull().any())
+print(X.isnull().any().any())
+# no. of missing values present 
+print(X.isnull().any().sum())
+print(X.isnull().sum())
+
+# to check the numeric variables
+numeric = list(X.select_dtypes(include=[np.number]))
+numeric.remove('MSSubClass')
+print('Here are the %s numeric variables : \n %s' %(len(numeric) , numeric))
+
+# to check the categorical variables
+object = list(X.select_dtypes(include=[np.object]))
+print('Here are the %s object variables : \n %s' %(len(object) , object)) 
+
+# Categorical variables
+def transform_variable(dataset):
+    dataset = X
+    copy = dataset
+    # Important categorical variables
+    copy.replace({'MSSubClass' : {
+        20	:'1-STORY 1946 & NEWER ALL STYLES' ,
+            30	:'1-STORY 1945 & OLDER' ,
+            40	 :    '1-STORY W/FINISHED ATTIC ALL AGES',
+            45	:'1-1/2 STORY - UNFINISHED ALL AGES' ,
+            50	:'1-1/2 STORY FINISHED ALL AGES' ,
+            60	:'2-STORY 1946 & NEWER' ,
+            70	 : '2-STORY 1945 & OLDER' ,
+            75	:'2-1/2 STORY ALL AGES' ,
+            80	:'SPLIT OR MULTI-LEVEL' ,
+            85	:'SPLIT FOYER',
+            90	:'DUPLEX - ALL STYLES AND AGES',
+           120	:'1-STORY PUD (Planned Unit Development) - 1946 & NEWER',
+           150	:'1-1/2 STORY PUD - ALL AGES',
+           160	:'2-STORY PUD - 1946 & NEWER',
+           180	:'PUD - MULTILEVEL - INCL SPLIT LEV/FOYER',
+           190	:'2 FAMILY CONVERSION - ALL STYLES AND AGES',
+    }} , inplace = True)
+    
+    # one hot encoding
+    columns = ['MSZoning','MSSubClass', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition']
+    for col_name in columns:
+             copy = pd.concat([copy , pd.get_dummies(copy[col_name] , prefix = col_name)] , axis =1)
+        copy = copy.drop(col_name , axis=1)
+    '''  
+    # ordinal variables transformation
+    quality = {'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0}
+    basement = { 'NA' : 0 , 'Unf' : 1 , 'LwQ' : 2 , 'Rec' : 3, 'BLQ' : 4 , 'ALQ' : 5 , 'GLQ' : 6 }
+    ordinal = {
+        'HeatingQC': quality,
+        'ExterQual': quality,
+        'ExterCond': quality,
+        'BsmtQual': quality,
+        'BsmtCond': quality,
+        'KitchenQual': quality,
+        'FireplaceQu': quality,
+        'GarageQual': quality,
+        'GarageCond': quality,
+        'PoolQC': quality,
+        'BsmtFinType1': basement,
+        'BsmtFinType2': basement,
+        'LandSlope':{'Gtl': 0 , 'Mod' : 1, 'Sev' :2},
+        'MasVnrType':{'None' : 0 , 'BrkCmn' : 0 , 'BrkFace' : 1, 'Stone' : 2},
+        'BsmtExposure': {'Gd' : 4 , 'Av' : 3 , 'Mn' : 2 , 'No' : 1, 'NA' : 0},
+        'CentralAir': { 'N' : 0, 'Y' : 1},
+        'GarageFinish':{'NA' : 0 , 'Unf' : 1, 'Rfn' : 2 , 'Fin' : 3},
+        'PavedDrive':{ 'N' : 0,'P' : 1, 'Y' : 2}
+    }
+    
+    for col_name , matching_map in ordinal.items():
+            copy[col_name] = copy[col_name].replace(matching_map , inplace=True)
+    '''
+       
+    return copy
+
+X = transform_variable(X)
+print(X.shape)
+
+'''
+result =pd.DataFrame()
+result['HeatingQC'] = X['HeatingQC'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result.head(8)
+result['ExterQual'] = X['ExterQual'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['ExterCond'] = X['ExterCond'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['BsmtQual'] = X['BsmtQual'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['BsmtCond'] = X['BsmtCond'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result[ 'KitchenQual'] = X[ 'KitchenQual'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['FireplaceQu'] = X['FireplaceQu'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result[ 'GarageQual'] = X[ 'GarageQual'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['GarageCond'] = X['GarageCond'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['PoolQC'] = X['PoolQC'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['BsmtFinType1'] = X['BsmtFinType1'].replace({ 'NA' : 0 , 'Unf' : 1 , 'LwQ' : 2 , 'Rec' : 3, 'BLQ' : 4 , 'ALQ' : 5 , 'GLQ' : 6 })
+result['BsmtFinType2'] = X['BsmtFinType2'].replace({ 'NA' : 0 , 'Unf' : 1 , 'LwQ' : 2 , 'Rec' : 3, 'BLQ' : 4 , 'ALQ' : 5 , 'GLQ' : 6 })
+result['LandSlope'] = X['LandSlope'].replace({'Gtl': 0 , 'Mod' : 1, 'Sev' :2})
+result['MasVnrType'] = X['MasVnrType'].replace({'None' : 0 , 'BrkCmn' : 0 , 'BrkFace' : 1, 'Stone' : 2})
+result['BsmtExposure'] = X['BsmtExposure'].replace({'Gd' : 4 , 'Av' : 3 , 'Mn' : 2 , 'No' : 1, 'NA' : 0})
+result['CentralAir'] = X['CentralAir'].replace({ 'N' : 0, 'Y' : 1})
+result['GarageFinish'] = X['GarageFinish'].replace({'NA' : 0 , 'Unf' : 1, 'Rfn' : 2 , 'Fin' : 3})
+result['PavedDrive'] = X['PavedDrive'].replace({ 'N' : 0,'P' : 1, 'Y' : 2})
+X['ExterQual'] = result['ExterQual']
+X['ExterCond'] = result['ExterCond'] 
+X['BsmtQual']=result['BsmtQual']
+X['BsmtCond']=result['BsmtCond']
+X[ 'KitchenQual']=result[ 'KitchenQual']
+X['FireplaceQu']=result['FireplaceQu']
+X[ 'GarageQual']=result[ 'GarageQual']
+X['GarageCond']=result['GarageCond']
+X['PoolQC']=result['PoolQC']
+X['BsmtFinType1']=result['BsmtFinType1']
+X['BsmtFinType2']=result['BsmtFinType2']
+X['LandSlope']=result['LandSlope'] 
+X['MasVnrType']=result['MasVnrType']
+X['BsmtExposure']=result['BsmtExposure']
+X['CentralAir']=result['CentralAir']
+X['GarageFinish']=result['GarageFinish']
+X['PavedDrive']=result['PavedDrive']
+'''
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = True)
+
+X_train =X_train.values
+# print(X_train.dtypes) :: to checkkk if the X_train is converted into numpy array or not [if error occur it is , if not it's not]
+X_test =X_test.values
+y_train=y_train.values
+y_test=y_test.values
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+# Fitting Decision Tree Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+y_pred
+print(y_pred.shape)
+y_test
+print(y_test.shape)
+
+# to check the accuracy of the model 
+# TRAINING SET
+print(regressor.score(X_train, y_train))       # 100
+# TEST SET 
+print(regressor.score(X_test, y_test))         # 69.87311295
---
+++ House_pricing/price_RFR.py
@@ -0,0 +1,339 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 12:44:28 2020
+
+@author: Swati
+"""
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Importing the dataset
+data = pd.read_csv(r'C:\Users\Swati\Downloads\data.csv')
+'''
+X = data.iloc[:, 1:-1].values
+y = data.iloc[:, -1].values
+'''
+X = data.iloc[:, 1:-1]
+y = data.iloc[:, -1]
+print(X.shape)
+
+# Conbining datasets
+data = pd.concat((X_train ,X_test)).reset_index(drop=True)
+print('The X_train set shape is : %s' %str(train.shape))
+print('The X_test set shape is : %s' %str(test.shape))
+print('The combined data set shape is : %s' %str(data.shape))
+
+
+# REMOVING columns FROM TRAINING SET 
+data.columns.get_loc('Condition1')
+data.columns.get_loc('GarageQual')
+data.columns.get_loc('Street')
+data.columns.get_loc('YearBuilt')
+'''
+X = np.delete(X,13 , 1 )
+X = np.delete(X,63 , 1 )
+X = np.delete(X,5 , 1 )
+X = np.delete(X,19 , 1 )
+# DELETE IS NUMPY FUNCTION 
+'''
+X = X.drop(['Condition1','Street','YearBuilt'] , axis=1)
+print(X.shape)
+print('The X set shape is : %s' %str(X.shape))
+print('The y set shape is : %s' %str(y.shape))
+print('The data set shape is : %s' %str(data.shape))
+
+'''
+# Determines all the unique element in the array
+np.unique(X[: , 2])
+
+# Determine the LENGTH of the unique element present in the array 
+len(np.unique(X[: , 17]))     
+X = np.delete(X, 17 , 1 )
+'''
+'''
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+
+X[:, 2] = labelencoder.fit_transform(X[:, 2])
+X[:, 2].reshape(-1, 1)
+onehotencoder = OneHotEncoder(categorical_features = [147])
+X = onehotencoder.fit_transform(X[:, 2]).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+'''
+
+'''
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+'''
+
+# Imputing the missing values
+missing =X.isna().sum(axis=0).sort_values(ascending=False)
+missing_value_columns = missing[missing>0]
+print('They are %s columns with missing values : \n%s ' %(missing_value_columns.count() , [(index , value) for (index , value) in missing_value_columns.iteritems()]))
+
+def impute_value(X):
+    dataset =X
+    dataset['PoolQC'].fillna('NA' , inplace = True)
+    dataset['MiscFeature'].fillna('NA' , inplace = True)
+    dataset['Alley'].fillna('NA' , inplace = True)
+    dataset['Fence'].fillna('NA' , inplace = True)
+    dataset['FireplaceQu'].fillna('NA' , inplace = True)
+    dataset['LotFrontage'].fillna(dataset['LotFrontage'].median() , inplace = True)
+    dataset['GarageCond'].fillna('NA' , inplace = True)
+    dataset['GarageType'].fillna('NA' , inplace = True)
+    dataset['GarageYrBlt'].fillna(dataset['GarageYrBlt'].median() , inplace = True)
+    dataset['GarageFinish'].fillna('NA' , inplace = True)
+    dataset['GarageQual'].fillna('NA' , inplace = True)
+    dataset['BsmtExposure'].fillna('NA' , inplace = True)
+    dataset['BsmtFinType2'].fillna('NA' , inplace = True)
+    dataset['BsmtFinType1'].fillna('NA' , inplace = True)
+    dataset['BsmtCond'].fillna('NA' , inplace = True)
+    dataset['BsmtQual'].fillna('NA' , inplace = True)
+    dataset['MasVnrArea'].fillna(dataset['MasVnrArea'].median() , inplace = True)
+    dataset['MasVnrType'].fillna('None' , inplace = True)
+    dataset['Electrical'].fillna('SBrkr' , inplace = True)
+    return dataset
+
+X = impute_value(X)
+
+# Cross checking the missing value
+missing =X.isna().sum(axis=0).sort_values(ascending=False)
+missing_value_columns = missing[missing>0]
+print('They are %s columns with missing values : \n%s ' %(missing_value_columns.count() , [(index , value) for (index , value) in missing_value_columns.iteritems()]))
+
+# to check the missing value is present or not
+print(X.isnull())
+print(X.isnull().any())
+print(X.isnull().any().any())
+# no. of missing values present 
+print(X.isnull().any().sum())
+print(X.isnull().sum())
+
+# to check the numeric variables
+numeric = list(X.select_dtypes(include=[np.number]))
+numeric.remove('MSSubClass')
+print('Here are the %s numeric variables : \n %s' %(len(numeric) , numeric))
+
+# to check the categorical variables
+object = list(X.select_dtypes(include=[np.object]))
+print('Here are the %s object variables : \n %s' %(len(object) , object)) 
+
+# Categorical variables
+def transform_variable(dataset):
+    dataset = X
+    copy = dataset
+    # Important categorical variables
+    copy.replace({'MSSubClass' : {
+        20	:'1-STORY 1946 & NEWER ALL STYLES' ,
+            30	:'1-STORY 1945 & OLDER' ,
+            40	 :    '1-STORY W/FINISHED ATTIC ALL AGES',
+            45	:'1-1/2 STORY - UNFINISHED ALL AGES' ,
+            50	:'1-1/2 STORY FINISHED ALL AGES' ,
+            60	:'2-STORY 1946 & NEWER' ,
+            70	 : '2-STORY 1945 & OLDER' ,
+            75	:'2-1/2 STORY ALL AGES' ,
+            80	:'SPLIT OR MULTI-LEVEL' ,
+            85	:'SPLIT FOYER',
+            90	:'DUPLEX - ALL STYLES AND AGES',
+           120	:'1-STORY PUD (Planned Unit Development) - 1946 & NEWER',
+           150	:'1-1/2 STORY PUD - ALL AGES',
+           160	:'2-STORY PUD - 1946 & NEWER',
+           180	:'PUD - MULTILEVEL - INCL SPLIT LEV/FOYER',
+           190	:'2 FAMILY CONVERSION - ALL STYLES AND AGES',
+    }} , inplace = True)
+    
+    # one hot encoding
+    columns = ['MSZoning','MSSubClass', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition']
+    for col_name in columns:
+        copy = pd.concat([copy , pd.get_dummies(copy[col_name] , prefix = col_name)] , axis =1)
+        copy = copy.drop(col_name , axis=1)
+    '''  
+    # ordinal variables transformation
+    quality = {'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0}
+    basement = { 'NA' : 0 , 'Unf' : 1 , 'LwQ' : 2 , 'Rec' : 3, 'BLQ' : 4 , 'ALQ' : 5 , 'GLQ' : 6 }
+    ordinal = {
+        'HeatingQC': quality,
+        'ExterQual': quality,
+        'ExterCond': quality,
+        'BsmtQual': quality,
+        'BsmtCond': quality,
+        'KitchenQual': quality,
+        'FireplaceQu': quality,
+        'GarageQual': quality,
+        'GarageCond': quality,
+        'PoolQC': quality,
+        'BsmtFinType1': basement,
+        'BsmtFinType2': basement,
+        'LandSlope':{'Gtl': 0 , 'Mod' : 1, 'Sev' :2},
+        'MasVnrType':{'None' : 0 , 'BrkCmn' : 0 , 'BrkFace' : 1, 'Stone' : 2},
+        'BsmtExposure': {'Gd' : 4 , 'Av' : 3 , 'Mn' : 2 , 'No' : 1, 'NA' : 0},
+        'CentralAir': { 'N' : 0, 'Y' : 1},
+        'GarageFinish':{'NA' : 0 , 'Unf' : 1, 'Rfn' : 2 , 'Fin' : 3},
+        'PavedDrive':{ 'N' : 0,'P' : 1, 'Y' : 2}
+    }
+    
+    for col_name , matching_map in ordinal.items():
+            copy[col_name] = copy[col_name].replace(matching_map , inplace=True)
+    '''
+       
+    return copy
+
+X = transform_variable(X)
+print(X.shape)
+
+'''
+result =pd.DataFrame()
+result['HeatingQC'] = X['HeatingQC'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result.head(8)
+result['ExterQual'] = X['ExterQual'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['ExterCond'] = X['ExterCond'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['BsmtQual'] = X['BsmtQual'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['BsmtCond'] = X['BsmtCond'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result[ 'KitchenQual'] = X[ 'KitchenQual'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['FireplaceQu'] = X['FireplaceQu'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result[ 'GarageQual'] = X[ 'GarageQual'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['GarageCond'] = X['GarageCond'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['PoolQC'] = X['PoolQC'].replace({'Ex' : 5 , 'Gd' : 4 , 'TA' : 3 , 'Po' : 1 , 'NA' : 0})
+result['BsmtFinType1'] = X['BsmtFinType1'].replace({ 'NA' : 0 , 'Unf' : 1 , 'LwQ' : 2 , 'Rec' : 3, 'BLQ' : 4 , 'ALQ' : 5 , 'GLQ' : 6 })
+result['BsmtFinType2'] = X['BsmtFinType2'].replace({ 'NA' : 0 , 'Unf' : 1 , 'LwQ' : 2 , 'Rec' : 3, 'BLQ' : 4 , 'ALQ' : 5 , 'GLQ' : 6 })
+result['LandSlope'] = X['LandSlope'].replace({'Gtl': 0 , 'Mod' : 1, 'Sev' :2})
+result['MasVnrType'] = X['MasVnrType'].replace({'None' : 0 , 'BrkCmn' : 0 , 'BrkFace' : 1, 'Stone' : 2})
+result['BsmtExposure'] = X['BsmtExposure'].replace({'Gd' : 4 , 'Av' : 3 , 'Mn' : 2 , 'No' : 1, 'NA' : 0})
+result['CentralAir'] = X['CentralAir'].replace({ 'N' : 0, 'Y' : 1})
+result['GarageFinish'] = X['GarageFinish'].replace({'NA' : 0 , 'Unf' : 1, 'Rfn' : 2 , 'Fin' : 3})
+result['PavedDrive'] = X['PavedDrive'].replace({ 'N' : 0,'P' : 1, 'Y' : 2})
+X['ExterQual'] = result['ExterQual']
+X['ExterCond'] = result['ExterCond'] 
+X['BsmtQual']=result['BsmtQual']
+X['BsmtCond']=result['BsmtCond']
+X[ 'KitchenQual']=result[ 'KitchenQual']
+X['FireplaceQu']=result['FireplaceQu']
+X[ 'GarageQual']=result[ 'GarageQual']
+X['GarageCond']=result['GarageCond']
+X['PoolQC']=result['PoolQC']
+X['BsmtFinType1']=result['BsmtFinType1']
+X['BsmtFinType2']=result['BsmtFinType2']
+X['LandSlope']=result['LandSlope'] 
+X['MasVnrType']=result['MasVnrType']
+X['BsmtExposure']=result['BsmtExposure']
+X['CentralAir']=result['CentralAir']
+X['GarageFinish']=result['GarageFinish']
+X['PavedDrive']=result['PavedDrive']
+'''
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = True)
+
+X_train =X_train.values
+# print(X_train.dtypes) :: to checkkk if the X_train is converted into numpy array or not [if error occur it is , if not it's not]
+X_test =X_test.values
+y_train=y_train.values
+y_test=y_test.values
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+# Fitting Random Forest Regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = True)
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+y_pred
+print(y_pred.shape)
+y_test
+print(y_test.shape)
+
+# to check the accuracy of the model 
+# TRAINING SET
+print(regressor.score(X_train, y_train))      # 97.76324962
+# TEST SET 
+print(regressor.score(X_test, y_test))        # 88.87353596
+
+
+'''
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)  
+X_test = sc_X.transform(X_test) 
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+
+
+
+# Updated Imputer
+from sklearn.impute import SimpleImputer
+missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
+missingvalues = missingvalues.fit(X[:, 1:3])
+X[:, 1:3]=missingvalues.transform(X[:, 1:3])
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+plt.scatter(X[: , -5] ,y ,  color = 'red')
+
+# -9                   -8 -7 couldn't convert             
+
+
+np.unique(X[: , -5])
+
+source_col_loc = dataset.columns.get_loc('OpenPorchSF')
+dataset['Porch'] =dataset.iloc[:, source_col_loc+1:source_col_loc+4].apply(
+        lambda X: ','.join(X.dropna.astype(str)), axis =1)
+dataset
+
+dataset1=   dataset.corr()
+fig =plt.figure()
+ax =fig.add_subplot(111)
+cax =ax.matshow(dataset1 , cmap ='coolwarm' , vmin=-1 , vmax=1)
+fig.colorbar(cax)
+ticks=np.arange(0 ,len(dataset.columns),1)
+ax.set_xticks(ticks)
+plt.set_xticks(rotation=90)
+ax.set_yticks(ticks)
+ax.set_xticklabels(dataset.columns)
+ax.set_yticklabels(dataset.columns)
+plt.show()
+
+
+dataset1=   dataset.corr()
+sns.heatmap(dataset1)
+
+'''
+
+
+''' CONDITION 1 , STREETS ,   YEAR BUILT , 
+
+
+'''
+
+
+'''
+RELATION 
+Exterior2nd Exterior1st
+ExterQual  ExterCond
+MasVnrType MasVnrArea
+OverallQual OverallCond
+BsmtExposure  BsmtQual
+BsmtFinType1 BsmtFinType2
+BsmtFinSF2 BsmtFinSF1 BsmtUnfSF TotalBsmtSF 
+GarageCars GarageArea
+'''
